76419aeaddc9196ac7027863df8bd1144de2df1c
Sam Magura <srmagura@gmail.com>
2023-06-14 19:34:35 -0400

    comments

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import "./style.css";
 
+// This code should be considered part of the host app. So it can't be modified.
+
 setInterval(() => {
   document.querySelector<HTMLElement>("#focusTrap input")!.focus();
 }, 1000);
+
+// CHALLENGE: Write code that allows the "FullContext ContentApp" input to
+// remain focused.
