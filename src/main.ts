43b4e7df205bfe936be170cf0bb495ff2ff6400a
Sam Magura <srmagura@gmail.com>
2023-06-14 19:38:14 -0400

    add example code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,60 @@
 import "./style.css";
 
+const focusTrapInput = document.querySelector<HTMLElement>("#focusTrap input")!;
+
 // This code should be considered part of the host app. So it can't be modified.
 
 setInterval(() => {
-  document.querySelector<HTMLElement>("#focusTrap input")!.focus();
+  focusTrapInput.focus();
 }, 1000);
 
 // CHALLENGE: Write code that allows the "FullContext ContentApp" input to
 // remain focused.
+
+focusTrapInput.addEventListener(
+  "focus",
+  (e) => {
+    console.log(
+      "[input focus capture] Preventing default and stopping immediate propagation..."
+    );
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { capture: true }
+);
+
+focusTrapInput.addEventListener(
+  "focusin",
+  (e) => {
+    console.log(
+      "[input focusin capture] Preventing default and stopping immediate propagation..."
+    );
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { capture: true }
+);
+
+window.addEventListener(
+  "focus",
+  (e) => {
+    console.log(
+      "[window focus capture] Preventing default and stopping immediate propagation..."
+    );
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { capture: true }
+);
+
+window.addEventListener(
+  "focusin",
+  (e) => {
+    console.log(
+      "[window focusin capture] Preventing default and stopping immediate propagation..."
+    );
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  { capture: true }
+);
